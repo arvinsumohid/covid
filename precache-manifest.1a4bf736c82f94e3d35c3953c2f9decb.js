14d954cdd6fd2e636c5f5fd9a47f2527f61ccbc5
arvinsumohid <arvinsumohid99@gmail.com>
2021-02-23 23:00:40 +0800

    Updates

---
+++ precache-manifest.1a4bf736c82f94e3d35c3953c2f9decb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c137959dacf5c5fc89b81c6ba4bc9b",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "5f00d8653ffe47283e2b",
+    "url": "/covid/static/css/main.902fef6a.chunk.css"
+  },
+  {
+    "revision": "fea1020dd359cf7f51d2",
+    "url": "/covid/static/js/2.252ed034.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid/static/js/2.252ed034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f00d8653ffe47283e2b",
+    "url": "/covid/static/js/main.6a5a610a.chunk.js"
+  },
+  {
+    "revision": "2538616d296fec9cb619",
+    "url": "/covid/static/js/runtime-main.6f8ac736.js"
+  }
+]);
